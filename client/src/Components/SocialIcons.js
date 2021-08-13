9a27255bbed6ae44e23e2b73af7b54bd2b41775e
pro335 <pawlbai124@gmail.com>
2021-08-13 06:27:43 +0400

    fix some issues v1.2 for no-data

--- client/src/Components/SocialIcons.js
+++ client/src/Components/SocialIcons.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import isValid from '../../utility/isValid';
+import isValid from '../utility/isValid';
 
 const SocialIcons = () => {
     
