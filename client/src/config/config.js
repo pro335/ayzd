8d0fc6247337c5ed272cb1aa4ce0d9ec1869cffd
pro335 <pawlbai124@gmail.com>
2021-09-10 14:51:32 +0400

    update bucket url

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -4,7 +4,7 @@ const config = {
     : {httpUrl :'http://localhost:8000'},
   REGION: "us-east-2",
   S3_BUCKET: "ayzd-storage",
-  bucket_url: "https://ayzd-storage.s3.us-east-2.amazonaws.com",
+  bucket_url: "https://ayzd-storage.fra1.digitaloceanspaces.com",
   general_avatar: "general_avatar.png",
   common_image: "common_image.png",
   TIME_INTERVAL: 1000*60*60,    // 1 hour,
