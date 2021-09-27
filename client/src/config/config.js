18b992d8282731828ec15d6d02b9b06cc9e9313a
pro335 <pawlbai124@gmail.com>
2021-09-27 21:47:25 +0400

    update google analytics tacking id

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -8,7 +8,7 @@ const config = {
   general_avatar: "general_avatar.png",
   common_image: "common_image.png",
   TIME_INTERVAL: 1000*60*60,    // 1 hour,
-  GOOGLE_ANALYTICS_TRACKING_ID: "G-R10EF983PR",
+  GOOGLE_ANALYTICS_TRACKING_ID: "G-SDDKN4GBXJ",
 };
 
 export default config;
